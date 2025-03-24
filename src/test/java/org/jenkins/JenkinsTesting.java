803b31555c6582dcc9ca9beaab82e084eca80ce6
JeyaSubashini0701 <subashiniinnaciyar@gmail.com>
2025-03-24 15:48:01 +0530

    first attempt to push

--- src/test/java/org/jenkins/JenkinsTesting.java
+++ src/test/java/org/jenkins/JenkinsTesting.java
@@ -3,5 +3,6 @@ package org.jenkins;
 public class JenkinsTesting {
    public static void main(String[] args) {
 	System.out.println("subashini project run successfully");
+	System.out.println("First attempt to push");
 }
 }
